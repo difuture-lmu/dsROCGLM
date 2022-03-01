12124cfd2048d63b4e011920f65bd4d27cecaa6b
schalkdaniel <d-schalk@t-online.de>
2022-03-01 11:39:20 +0100

    fix l2sens

--- R/l2sens.R
+++ R/l2sens.R
@@ -10,11 +10,12 @@
 #' @param col_names (`character(1)`) Subset of columns used to find adjacent inputs.
 #' @param norm (`function()`) Function to calculate the differnece between the
 #'   scores of adjacent inputs (default = Euclidean norm).
+#' @param drop_on_error (`logical(1L)`) NA is returned if an error occurs on a server.
 #' @return List with maximal l2 sensitivity, indices of inputs that are
 #'   used to calculate the maximal l2 sensitivity, and the number of adjacent inputs.
 #' @author Daniel S.
 #' @export
-l2sens = function(dat_name, scores_name, nbreaks = NULL, col_names = NULL, norm = diff) {
+l2sens = function(dat_name, scores_name, nbreaks = NULL, col_names = NULL, norm = diff, drop_on_error = TRUE) {
   if (checkmate::testCharacter(dat_name, len = 1L))
     dat = eval(parse(text = dat_name))
   else
@@ -39,32 +40,41 @@ l2sens = function(dat_name, scores_name, nbreaks = NULL, col_names = NULL, norm 
   checkmate::assertCount(nbreaks, null.ok = TRUE)
   checkmate::assertCharacter(cols, null.ok = TRUE)
   checkmate::assertFunction(norm)
+  checkmate::assertLogical(drop_on_error, len = 1L)
 
-  if (is.null(nbreaks)) nbreaks = floor(nrow(dat) / 3)
-  if (is.null(cols)) cols = colnames(dat)
-  dtmp = dat[, cols, drop = FALSE]
-  int_representation = do.call(cbind, lapply(dtmp, function(cl) {
-    if (is.numeric(cl)) {
-      xh = graphics::hist(cl, plot = FALSE, breaks = nbreaks)$breaks
-      return(vapply(cl, function(x) which.min(xh <= x) - 1, FUN.VALUE = numeric(1L)))
-    } else {
-      return(stats::model.matrix(~ 0 + cl))
-    }
-  }))
-  l1n = as.matrix(dist(int_representation, method = "manhattan"))
-  mdist = min(l1n[l1n > 0])
-  if (mdist > 1)
-    warning("Was not able to found direct neighbour. Found l1 norm of ", mdist, " as closest inputs.")
+  e = try(expr = {
+    if (is.null(nbreaks)) nbreaks = floor(nrow(dat) / 3)
+    if (is.null(cols)) cols = colnames(dat)
+    dtmp = dat[, cols, drop = FALSE]
+    int_representation = do.call(cbind, lapply(dtmp, function(cl) {
+      if (is.numeric(cl)) {
+        xh = graphics::hist(cl, plot = FALSE, breaks = nbreaks)$breaks
+        return(vapply(cl, function(x) which.min(xh <= x) - 1, FUN.VALUE = numeric(1L)))
+      } else {
+        return(stats::model.matrix(~ 0 + cl))
+      }
+    }))
+    l1n = as.matrix(dist(int_representation, method = "manhattan"))
+    mdist = min(l1n[l1n > 0])
+    if (mdist > 1)
+      warning("Was not able to found direct neighbour. Found l1 norm of ", mdist, " as closest inputs.")
 
-  idx     = which(l1n == mdist, arr.ind = TRUE)
-  l2senss = apply(idx, 1, function(i) norm(scores[i]))
-  ml2sens = max(l2senss)
+    idx     = which(l1n == mdist, arr.ind = TRUE)
+    l2senss = apply(idx, 1, function(i) norm(scores[i]))
+    ml2sens = max(l2senss)
 
-  if (ml2sens == 0)
-    stop("No reasonable l2 sensitivity found! Try to decrease the number of breaks (current = ", nbreaks, ")")
+    if (ml2sens == 0)
+      stop("No reasonable l2 sensitivity found! Try to decrease the number of breaks (current = ", nbreaks, ")")
+    list(l2sens = e, nl1n = nrow(idx) / 2, l1n = mdist,
+      idx = unname(idx[which.max(l2senss), ]))
+  }, silent = TRUE)
 
-  return(list(l2sens = ml2sens, nl1n = nrow(idx) / 2, l1n = mdist,
-    idx = unname(idx[which.max(l2senss), ])))
+  if (inherits(e, "try-error")) {
+    warning("Additional error occurs: ", attributes(e)$condition$message)
+    return(list(l2sens = NA, nl1n = NA, l2n = NA, idx = NA))
+  } else {
+    return(e)
+  }
 }
 
 #'
@@ -78,15 +88,17 @@ l2sens = function(dat_name, scores_name, nbreaks = NULL, col_names = NULL, norm 
 #' @param nbreaks (`integer(1L)`) Number of breaks used for the histogram
 #'   (default = nrow(dat) / 3).
 #' @param cols (`character()`) Subset of columns used to find adjacent inputs.
+#' @param drop_on_error (`logical(1L)`) NA is returned if an error occurs on a server.
 #' @return List with maximal l2 sensitivity, indices of inputs that are
 #'   used to calculate the maximal l2 sensitivity, and the number of adjacent inputs.
 #' @author Daniel S.
 #' @export
-dsL2Sens = function(connections, dat_name, pred_name, nbreaks = NULL, cols = NULL) {
+dsL2Sens = function(connections, dat_name, pred_name, nbreaks = NULL, cols = NULL, drop_on_error = TRUE) {
   checkmate::assertCharacter(dat_name, len = 1L)
   checkmate::assertCharacter(pred_name, len = 1L)
   checkmate::assertCount(nbreaks, null.ok = TRUE)
   checkmate::assertCharacter(cols, null.ok = TRUE)
+  checkmate::assertLogical(drop_on_error, len = 1L)
 
   if (is.null(nbreaks)) {
     ntotal = ds.dim(dat_name, type = "combined", datasources = connections)
@@ -95,7 +107,7 @@ dsL2Sens = function(connections, dat_name, pred_name, nbreaks = NULL, cols = NUL
 
   xXcols = cols
   pushObject(connections, xXcols)
-  f = paste0("l2sens(\"", dat_name, "\", \"", pred_name, "\", ", nbreaks, ", \"xXcols\")")
+  f = paste0("l2sens(\"", dat_name, "\", \"", pred_name, "\", ", nbreaks, ", \"xXcols\", ", drop_on_error, ")")
 
   ll_l2s = DSI::datashield.aggregate(conns = connections, f)
   l2s = as.data.frame(do.call(rbind, lapply(ll_l2s, function(x) {
